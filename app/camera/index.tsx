dd37c7476d0fa72b5a5921abe54a98a10950072a
Brayden O'Neal <braydenoneal@protonmail.com>
2024-12-04 21:23:22 -0600

    Add setup page

---
+++ app/camera/index.tsx
@@ -0,0 +1,112 @@
+import { CameraView, useCameraPermissions } from 'expo-camera'
+import { Image, Platform, View } from 'react-native'
+import { useRef, useState } from 'react'
+
+import { getUrl } from '@/lib/url'
+import { Button } from '~/components/ui/button'
+import { Text } from '~/components/ui/text'
+import { SlidersHorizontal } from '~/lib/icons/SlidersHorizontal'
+import { ArrowLeft } from '~/lib/icons/ArrowLeft'
+import { Link } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export default function Camera() {
+    const [permission, requestPermission] = useCameraPermissions()
+    const [image, setImage] = useState<string>()
+
+    const cameraRef = useRef<CameraView>(null)
+
+    if (!permission) {
+        return <View/>
+    }
+
+    if (!permission.granted) {
+        return (
+            <View className={'flex-1'}>
+                <Text>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission}><Text>Grant permission</Text></Button>
+            </View>
+        )
+    }
+
+    async function takePicture() {
+        if (cameraRef.current) {
+            const data = await cameraRef.current.takePictureAsync()
+
+            if (data) {
+                setImage(data.uri)
+            }
+        }
+    }
+
+    async function sendImage() {
+        const body = new FormData()
+        // @ts-ignore
+        body.append('image', {
+            uri: Platform.OS === 'ios' ? image?.replace('file://', '') : image,
+            name: 'photo.jpg',
+            type: 'image/jpeg',
+        })
+
+        const cvdType = await AsyncStorage.getItem('cvdType')
+        body.append('cvd_type', cvdType || '')
+
+        try {
+            const url = await getUrl()
+
+            const response = await fetch(url + '/image', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                body,
+            })
+
+            if (response.ok) {
+                const image = await response.blob()
+
+                const fileReaderInstance = new FileReader()
+                fileReaderInstance.readAsDataURL(image)
+
+                fileReaderInstance.onload = () => {
+                    const vtt_data = fileReaderInstance.result
+                    setImage(vtt_data?.toString())
+                }
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async function resetImage() {
+        setImage(undefined)
+    }
+
+    if (image) {
+        return (
+            <View className={'flex-1'}>
+                <View className={'flex-1 w-full'}>
+                    <Image className={'flex-1 w-full h-full'} source={{ uri: image }}/>
+                </View>
+                <View className={'w-full p-4 flex-row gap-4 justify-between items-center'}>
+                    <Button className={''} onPress={sendImage}><Text>Upload image</Text></Button>
+                    <Button className={''} size={'icon'} variant={'secondary'} onPress={resetImage}><ArrowLeft className={'text-primary'}/></Button>
+                </View>
+            </View>
+        )
+    }
+
+    return (
+        <View className={'flex-1'}>
+            <View className={'flex-1 w-full'}>
+                <CameraView style={{ flexGrow: 1, width: '100%', height: '100%' }} ref={cameraRef}/>
+            </View>
+            <View className={'w-full p-4 flex-row gap-4 justify-between items-center'}>
+                <Button className={''} onPress={takePicture}><Text>Take picture</Text></Button>
+                <Link href={'/'} asChild>
+                    <Button className={''} size={'icon'} variant={'secondary'}><SlidersHorizontal className={'text-primary'}/></Button>
+                </Link>
+            </View>
+        </View>
+    )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,105 +1,53 @@
-import { CameraView, useCameraPermissions } from 'expo-camera'
-import { Image, Platform, View } from 'react-native'
-import { useRef, useState } from 'react'
+import { View } from 'react-native'
+import { useState } from 'react'
 
-import { getUrl } from '@/lib/url'
 import { Button } from '~/components/ui/button'
 import { Text } from '~/components/ui/text'
-import { SlidersHorizontal } from '~/lib/icons/SlidersHorizontal'
-import { ArrowLeft } from '~/lib/icons/ArrowLeft'
+import { RadioGroup, RadioGroupItem } from '~/components/ui/radio-group'
+import { Label } from '~/components/ui/label'
+import { router } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function App() {
-    const [permission, requestPermission] = useCameraPermissions()
-    const [image, setImage] = useState<string>()
+    const [value, setValue] = useState('Protanopia')
 
-    const cameraRef = useRef<CameraView>(null)
-
-    if (!permission) {
-        return <View/>
-    }
-
-    if (!permission.granted) {
-        return (
-            <View className={'flex-1'}>
-                <Text>We need your permission to show the camera</Text>
-                <Button onPress={requestPermission}><Text>Grant permission</Text></Button>
-            </View>
-        )
-    }
-
-    async function takePicture() {
-        if (cameraRef.current) {
-            const data = await cameraRef.current.takePictureAsync()
-
-            if (data) {
-                setImage(data.uri)
-            }
+    function onLabelPress(label: string) {
+        return () => {
+            setValue(label)
         }
     }
 
-    async function sendImage() {
-        const body = new FormData()
-        // @ts-ignore
-        body.append('image', {
-            uri: Platform.OS === 'ios' ? image?.replace('file://', '') : image,
-            name: 'photo.jpg',
-            type: 'image/jpeg',
-        })
-
-        try {
-            const url = await getUrl()
-
-            const response = await fetch(url + '/image', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-                body,
-            })
-
-            if (response.ok) {
-                const image = await response.blob()
-
-                const fileReaderInstance = new FileReader()
-                fileReaderInstance.readAsDataURL(image)
-
-                fileReaderInstance.onload = () => {
-                    const vtt_data = fileReaderInstance.result
-                    setImage(vtt_data?.toString())
-                }
-            }
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
-    async function resetImage() {
-        setImage(undefined)
-    }
-
-    if (image) {
-        return (
-            <View className={'flex-1'}>
-                <View className={'flex-1 w-full'}>
-                    <Image className={'flex-1 w-full h-full'} source={{ uri: image }}/>
-                </View>
-                <View className={'w-full p-4 flex-row gap-4 justify-between items-center'}>
-                    <Button className={''} onPress={sendImage}><Text>Upload image</Text></Button>
-                    <Button className={''} size={'icon'} variant={'secondary'} onPress={resetImage}><ArrowLeft className={'text-primary'}/></Button>
-                </View>
-            </View>
-        )
+    async function onContinue() {
+        await AsyncStorage.setItem('cvdType', value)
+        router.replace('/camera')
     }
 
     return (
-        <View className={'flex-1'}>
-            <View className={'flex-1 w-full'}>
-                <CameraView style={{ flexGrow: 1, width: '100%', height: '100%' }} ref={cameraRef}/>
+        <View className={'flex-1 p-6 gap-8 flex-col'}>
+            <Text className={'text-lg'}>What type of color vision deficiency do you have?</Text>
+            <RadioGroup value={value} onValueChange={setValue} className='gap-3'>
+                <RadioGroupItemWithLabel value='Protanopia' onLabelPress={onLabelPress('Protanopia')} />
+                <RadioGroupItemWithLabel value='Deuteranopia' onLabelPress={onLabelPress('Deuteranopia')} />
+                <RadioGroupItemWithLabel value='Tritanopia' onLabelPress={onLabelPress('Tritanopia')} />
+                <RadioGroupItemWithLabel value='Achromatopsia' onLabelPress={onLabelPress('Achromatopsia')} />
+            </RadioGroup>
+            <View className={'flex-1 justify-end'}>
+                <Button onPress={onContinue} className={''}><Text>Continue</Text></Button>
             </View>
-            <View className={'w-full p-4 flex-row gap-4 justify-between items-center'}>
-                <Button className={''} onPress={takePicture}><Text>Take picture</Text></Button>
-                <Button className={''} size={'icon'} variant={'secondary'}><SlidersHorizontal className={'text-primary'}/></Button>
-            </View>
+        </View>
+    )
+}
+
+function RadioGroupItemWithLabel({ value, onLabelPress }: {
+    value: string;
+    onLabelPress: () => void;
+}) {
+    return (
+        <View className={'flex-row gap-2 items-center'}>
+            <RadioGroupItem aria-labelledby={`label-for-${value}`} value={value}/>
+            <Label nativeID={`label-for-${value}`} onPress={onLabelPress}>
+                {value}
+            </Label>
         </View>
     )
 }
